0c84854c4e582ca97cea7bd99f6ddb5acf9ec853
MAC <miguelangelcabrera1@hotmail.com>
2020-07-05 12:20:47 +0200

    First implementation of my script

---
+++ main.py
@@ -0,0 +1,34 @@
+import requests
+import json
+import base64
+
+#read credentials from file
+cred_file = open("./credentials.txt", 'r')
+user = cred_file.readline()
+psw = cred_file.readline()
+
+credentials = user + ':' + psw
+
+token = base64.b64encode(credentials.encode())
+
+url_srcdest = "http://localhost/wp-json/wp/v2/posts"
+
+headers = {'Content-Type': 'application/json', 
+         'Authorization': 'Basic ' + token.decode('utf-8')
+         }
+html_file = open("/Users/MAC/Library/Mobile Documents/com~apple~CloudDocs/Porno Mossegalapoma/programa_tipic_nomes_body.html",'r')
+contents = html_file.read()
+author_id_tomas = '1'
+post = \
+    {
+        "title":"Testing via API via Python3",
+        "content":contents,
+        "status": "publish",
+        "comment_status" : "open" ,
+        "password" : "1234",
+
+    }
+
+response = requests.post(url_srcdest, json=post, headers=headers)
+
+print(response)
